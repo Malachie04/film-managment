0561e613bbbabd18f65c40784cbd9f77de6c1c6a
Malachie04 <malakimalachie@gmail.com>
2025-06-03 11:34:15 +0200

    11:33Commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,71 +2,27 @@
 const apikey='6631e5f1dc96088e0d26b86da29b5b6a';
 let url='';
 let filmdetail ='';
+let imgUrl="https://image.tmdb.org/t/p/w500";
 //Object
 // const swipeHtml=document.querySelector('.swiper-wrapper');
 // const buttontHtml=document.querySelector('button');
+const wrapperFimlHTML=document.querySelector('.container-bottom');
+const buttons=document.querySelector('.buttons');
 
+const closepopupHTML=document.querySelector('.close');
+const imagepopuHTML=document.querySelector('.imagedupopup');
+const titreserieHTML=document.querySelector('.title-de-la-serie');
+const overviewHtml=document.querySelector('.overview');
+const popupHTML=document.querySelector('.popup');
 // const valueInput=document.querySelector('.input-text');
 //Function
 
 
 
-const getFilmyTitle=async (title)=>{
-    const apikey='6631e5f1dc96088e0d26b86da29b5b6a';
-    url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(title)}&include_adult=false&language=en-US&page=1&api_key=${apikey}`;
-    try {
-        let data=await fetch(url);
-        let result = await data.json();
-        return(result)
-    } catch (error) {
-        return error;
-    }
-}
-
-
-
-
-async function  dispalay(titre){
-    myrest= await getFilmyTitle(titre);
-    let res =myrest.results
-
-    swipeHtml.innerHTML=``;
-    console.log(res);
-    for(let film of res){
-        let titre=film.original_title
-        let image='';
-        if(film.poster_path)
-        image=`https://image.tmdb.org/t/p/w500${film.poster_path}`;
-        else{
-            image=`https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-300x450.jpg`;
-        }
-
-       
-
-        let slide=document.createElement('div');
-        slide.classList.add('swiper-slide');
-
-        let content=`
-            <div class="top">
-                <h2 class="title">${titre}</h2>
-            </div>
-            <div class="bottom">
-                <img src="${image}" alt="">
-            </div>
-        `;
-
-        slide.innerHTML=content;
-
-        swipeHtml.append(slide)
-        
-
-    }
-}
-
-
 
 // dispalay();
 
+displayAll('top_rated');
 
 async function getSeriesByList(list) {
     url=`https://api.themoviedb.org/3/tv/${list}?api_key=${apikey}&language=fr-FR&page=1`;
@@ -79,40 +35,103 @@ async function getSeriesByList(list) {
     }
 }
 
-
-
-
-async function displayAllTry(){
-    let result=await getSeriesByList('popular');
-    let myrest=result.results;
-
-
-    filmdetail=[myrest.original_title,myrest.poster_path,myrest.popularity]
-
-    localStorage.setItem(myrest.id,JSON.stringify(filmdetail));
-
-    console.log(myrest);
-    
-}
-
-
-async function displayAll() {
+async function displayAll(categoy) {
     localStorage.clear();
-    let result = await getSeriesByList('top_rated');
+    wrapperFimlHTML.innerHTML=``;
+    let result = await getSeriesByList(categoy);
     let myrest = result.results;
+    let fimlHTML;
 
     myrest.forEach(serie => {
         const filmdetail = [
             serie.name,          // ou serie.original_name
-            `https://image.tmdb.org/t/p/w500${serie.poster_path}`,
+            `${imgUrl}${serie.poster_path}`,
             serie.popularity,
             serie.overview
         ];
         localStorage.setItem(serie.id, JSON.stringify(filmdetail));
     });
-
-    console.log(myrest);
+    for(let film of myrest){
+        fimlHTML=`
+            <div class="card" data-id=${film.id}>
+                <h2 class="title">${film.original_name}</h2>
+                <img src="${imgUrl}${film.poster_path}" alt="image">
+                <div class="popularity"><span>${film.popularity}</span></div>
+            </div>
+        `;
+        wrapperFimlHTML.innerHTML+=fimlHTML;
+    }
+    
 }
 
 
 // displayAll();
+
+//Les buttons
+
+// buttons.addEventListener('click', (event) => {
+//     const elementClicked = event.target;
+
+//     if (elementClicked.tagName !== 'BUTTON') return;
+
+//     // elementClicked.classList.toggle('active');
+
+
+//     let nomClasse = elementClicked.className;
+
+
+//     if (nomClasse) {
+//         displayAll(nomClasse);
+//     }
+//     elementClicked.classList.add('active');
+// });
+
+
+
+
+buttons.addEventListener('click', (event) => {
+    const elementClicked = event.target;
+
+   
+    if (elementClicked.tagName !== 'BUTTON') return;
+
+   
+    const allButtons = buttons.querySelectorAll('button');
+    allButtons.forEach(btn => btn.classList.remove('active'));
+
+   
+    elementClicked.classList.add('active');
+
+   
+    const nomClasse = elementClicked.className.replace('active', '').trim();
+
+    if (nomClasse) {
+        displayAll(nomClasse); 
+    }
+});
+
+
+wrapperFimlHTML.addEventListener('click', (event) => {
+  const cardElement = event.target.closest('.card');
+
+  if (!cardElement || !wrapperFimlHTML.contains(cardElement)) return;
+
+  const idElement = cardElement.dataset.id;
+  const cardInfo = JSON.parse(localStorage.getItem(idElement));
+
+
+  console.log(cardInfo);
+  
+  if (cardInfo) {
+    imagepopuHTML.src = cardInfo[1];
+    titreserieHTML.innerHTML = cardInfo[0];
+    overviewHtml.innerHTML = cardInfo[3];
+    popupHTML.style.display='flex';
+  }
+});
+
+
+closepopupHTML.addEventListener('click',(event)=>{
+    event.preventDefault();
+    popupHTML.style.display='none';
+});
